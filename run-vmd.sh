e5cefb97e030513c538de384ba389a2f5127ba30
pkelich <61854834+pkelich@users.noreply.github.com>
2022-01-31 16:20:12 -0700

    Update run-vmd.sh

--- run-vmd.sh
+++ run-vmd.sh
@@ -2,8 +2,8 @@
 
 for i in *pat.tcl
 do
-	vmd -dispdev text -e $i > tmp-measuared
-	echo `head -1 $i |  grep -o -P '(?<=pdb ).*(?=.pdb)' && echo ',' && echo "$i" | grep -o -P '(?<=measure-).*(?=-pat.tcl)' && echo ',' && awk '($1+0)' tmp-measuared | sed 's/ /,/g'` >> final.csv
+	vmd -dispdev text -e $i > tmp-measured
+	echo `head -1 $i |  grep -o -P '(?<=pdb ).*(?=.pdb)' && echo ',' && echo "$i" | grep -o -P '(?<=measure-).*(?=-pat.tcl)' && echo ',' && awk '($1+0)' tmp-measured | sed 's/ /,/g'` >> final.csv
 	rm $i
 
 done
